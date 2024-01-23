50031c4cd1e3684e7097170ba8ba05b968bf68c7
sillysillyman <sillysillyman.cs@gmail.com>
2024-01-24 06:58:28 +0900

    sine.py finished

--- pp2/sine.py
+++ pp2/sine.py
@@ -4,19 +4,33 @@
 
 import math
 
+
 def sine(x):
-  raise NotImplementedError
+    # raise NotImplementedError
+    if -0.01 < x < 0.01:
+        return x - x**3 / 6
+    return 2 * sine(x / 2) * cosine(x / 2)
+
 
 def cosine(x):
-  raise NotImplementedError
+    # raise NotImplementedError
+    if -0.01 < x < 0.01:
+        return 1 - x**2 / 2
+    return 1 - 2 * sine(x / 2) ** 2
+
 
 def tabulate(a, b, step):
-  print("%5s : %-10s %-10s %-10s %-10s" % ("x", "sine(x)", "cosine(x)",
-                                      "math.sin(x)", "math.cos(x)"))
-  for i in range(a, b+1):
-    x = step * i
-    print("%5g : %-10g %-10g %-10g %-10g" %
-          (x, sine(x), cosine(x), math.sin(x), math.cos(x)))
+    print(
+        "%5s : %-10s %-10s %-10s %-10s"
+        % ("x", "sine(x)", "cosine(x)", "math.sin(x)", "math.cos(x)")
+    )
+    for i in range(a, b + 1):
+        x = step * i
+        print(
+            "%5g : %-10g %-10g %-10g %-10g"
+            % (x, sine(x), cosine(x), math.sin(x), math.cos(x))
+        )
+
 
 if __name__ == "__main__":
-  tabulate(-20, 20, 0.1)
+    tabulate(-20, 20, 0.1)
