facdafea56bb10a52e93753047b9ea8f771e7530
sillysillyman <sillysillyman.cs@gmail.com>
2024-02-05 22:23:21 +0900

    clockwise hanoi passed

--- pp2/clockwise.py
+++ pp2/clockwise.py
@@ -10,22 +10,26 @@ import sys
 # Postcondition:
 #   - n smallest disks are the top disks on pole destination.
 
+
 def hanoi_cw(n, source, destination, spare):
-  if n == 1:
-    print("Move disk 1 from %s to %s" % (source, destination))
-  else:
-    # The following part is wrong (not using clockwise movements)!
-    hanoi_cw(n-1, source, spare, destination)
-    print("Move disk %d from %s to %s" % (n, source, destination))
-    hanoi_cw(n-1, spare, destination, source)
+    if n == 1:
+        print("Move disk 1 from %s to %s" % (source, destination))
+    else:
+        # The following part is wrong (not using clockwise movements)!
+        hanoi_cw(n - 1, source, destination, spare)
+        hanoi_cw(n - 1, destination, spare, source)
+        print("Move disk %d from %s to %s" % (n, source, destination))
+        hanoi_cw(n - 1, spare, source, destination)
+        hanoi_cw(n - 1, source, destination, spare)
+
 
 # --------------------------------------------------------------------
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Missing argument")
-    sys.exit(1)
-  n = int(sys.argv[1])
-  hanoi_cw(n, 'A', 'B', 'C')
+    if len(sys.argv) != 2:
+        print("Missing argument")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    hanoi_cw(n, "A", "B", "C")
 
 # --------------------------------------------------------------------
